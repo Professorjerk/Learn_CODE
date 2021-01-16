cefcc19f2764a72b8c87927f5dff7acf49a1e197
Professorjerk <77491944+Professorjerk@users.noreply.github.com>
2021-01-16 19:49:38 +0530

    Update q1.cpp

--- Questions/Arrays/Basic/q1.cpp
+++ Questions/Arrays/Basic/q1.cpp
@@ -7,7 +7,10 @@ Take input from the user for these 'n' elements and print the entire array after
 using namespace std;
 
 int main()
-{
-
+{int n ;
+cin>>n;
+int a[n];
+for(int i =0 ; i<n-1; i++) {cin>>a[i]; 
+} for(int i =0 ; i<n-1; i++){cout<<a[i]; } 
     return 0;
 }
